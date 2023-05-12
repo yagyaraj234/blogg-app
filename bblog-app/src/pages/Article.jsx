ca64120bde91832887ae4593813bbdf01d2e79d1
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-12 17:15:49 +0530

    frontend-connected-with-backend

--- bblog-app/src/pages/Article.jsx
+++ bblog-app/src/pages/Article.jsx
@@ -10,12 +10,12 @@ import axios from 'axios'
 
 const Article = () => {
   const { articleId } = useParams();
-  const [articleInfo, setArticleInfo] = useState({ likes: 0, comments: [] })
+  const [articleInfo, setArticleInfo] = useState({ likes: 1, comments: [] })
 
   useEffect(() => {
     const loadArticleInfo = async () => {
       const response = await axios.get(`/api/articles/${articleId}`)
-      const newArticleInfo=response.data;
+      const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     }
     loadArticleInfo();
@@ -33,26 +33,27 @@ const Article = () => {
       <h1 className='text-5xl  font-sans text-black uppercase mb-4 '>{article.title}</h1>
 
       {article.content.map(paragraph => (<p key={paragraph} className='py-2'>{paragraph}</p>))}
+
+      {/* // interaction icons */}
       <div className='flex flex-row justify-between  '>
         < div className='flex'>
           <HeartIcon className='heroicons mr-4 ' />
           <ChatBubbleOvalLeftIcon className='heroicons mr-4' />
           <ShareIcon className='heroicons' />
         </div>
+
+        {/* Details  */}
         <div className='flex flex-row profile'>
-          <h2>{article.user}</h2>
-          <button className=' border border-black px-3 rounded-lg  text-sm  ml-5'  >{article.follow ? 'Following' : 'Follow'}</button>
+          <h2>{articleInfo.user}</h2>
+          <button className=' border border-black px-3 rounded-lg  text-sm  ml-5'  >{articleInfo.follow ? 'Following' : 'Follow'}</button>
         </div>
       </div>
       <div className="flex flex-row">
         <Link to='/likes'><p className='mr-1'>{articleInfo.likes} like(s)</p></Link>
 
         <p className='mr-1'></p>
-        <Link to='/comments'><p className='mr-1'>{articleInfo.comments.length} comment(s)</p></Link>
+        <p to='/comments' className='mr-1'>{articleInfo.comments.length} comment(s)</p>
       </div>
-
-
-
 
 
     </div>
