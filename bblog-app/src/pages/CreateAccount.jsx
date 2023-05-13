cf9f54ebdc76dde0defbbe665a8b60685b3e6715
Yagyaraj Lodhi <work.yagyaraj@gmail.com>
2023-05-13 15:29:41 +0530

    login-register page added

--- bblog-app/src/pages/CreateAccount.jsx
+++ bblog-app/src/pages/CreateAccount.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CreateAccount = () => {
-  return (
-    <div>CreateAccount</div>
-  )
+    return (
+        < div className=' m-auto flex flex-col pt-5 w-2/6 '>
+        <h1 className='text-3xl text-center mb-5'>Create a New Account</h1>
+        <label  className='text-lg'>Name</label>
+        <input className='my-1 py-1 border rounded outline-none border-2' type="text" placeholder='Enter your name'/>
+        <label  className='text-lg'>Email</label>
+        <input className='my-1 py-1 border rounded outline-none border-2' type="text" placeholder='Enter you mail'/>
+        
+        <label className='text-lg mt-2'>Password</label>
+        <input className='border rounded outline-none border-2 py-1 my-1' type="text" placeholder='Password'/>
+        <p className='text-xs '>Already Have Account ? <Link className='text-blue-500' to='/login'>Login Here.</Link></p>
+    
+        <button className='py-1 mt-5 border rounded hover:bg-gray-400 '>Register Now</button>
+    
+        </div>
+      )
 }
 
 export default CreateAccount
--- bblog-app/src/pages/LoginPage.jsx
+++ bblog-app/src/pages/LoginPage.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import {useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import {getAuth,signInWithEmailAndPassword} from 'firebase/auth'
+
+
 
 const LoginPage = () => {
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    
+    const [error,setError]=useState('');
+    const navigate =useNavigate();
+    
+    const logIn = async () =>{
+      
+      try{  await signInWithEmailAndPassword(getAuth(),email,password);
+        navigate('/articles');
+      } catch(e){
+        setError(e.message);
+      }
+    }
+
   return (
-    <>
-    <h1>Login Now</h1>
+    < div className=' m-auto flex flex-col pt-5 w-2/6 '>
 
-    <input type="text" placeholder='Name'/>
-    <input type="text" placeholder='Email'/>
-    <input type="text" placeholder='Passowrd'/>
+    <h1 className='text-3xl text-center mb-5'>Login Your Account</h1>
+    {error && <p >{error}</p>}
+    <label  className='text-lg'>Email</label>
+    <input  className='my-1 py-1 border rounded outline-none border-2' type="text" placeholder='Email' value={email}
+    onChange={e =>setEmail(e.target.value)}/>
+    
+    <label className='text-lg mt-2'>Password</label>
+    <input className='border rounded outline-none border-2 py-1 my-1' type="password" value={password} placeholder='Password'onChange={e =>setPassword(e.target.value)}/>
+    <p className='text-xs '>Don't Have Account ? <Link className='text-blue-500' to='/register'>Register Here.</Link></p>
 
-    </>
+    <button className='py-1 mt-5 border rounded hover:bg-gray-400 ' onClick={logIn}>Login Now</button>
+
+    </div>
   )
 }
 
